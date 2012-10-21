80d07d6bf5be143db4f7962b7760c7a16f1de006
Sam Bosley <sboz88@gmail.com>
2012-10-20 18:02:17 -0700

    Solved problem 3

--- problem3.rb
+++ problem3.rb
@@ -1,9 +1,19 @@
-require './math_ext.rb'
+require 'mathn.rb'
 def largest_prime_factor(n)
-	primes = SieveOfEra.primes(Math.sqrt(n).floor)
-	primes.reverse_each {|p|
-		return p if n % p == 0
-	}
-	1
+	p = Prime.new
+	upper = Math.sqrt(n)
+	largest = 1
+	while n > 1
+		nextP = p.next
+		break if nextP > upper
+		if n % nextP == 0
+			puts "FOUND FACTOR #{nextP}"
+			largest = nextP
+			while n % nextP == 0
+				n = n / nextP
+			end
+		end
+	end
+	largest
 end
 	
