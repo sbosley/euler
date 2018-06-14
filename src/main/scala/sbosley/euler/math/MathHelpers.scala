77b4f34d6084ce49079d9a67c7b13cdc21d35a99
Sam Bosley <sboz88@gmail.com>
2018-06-14 08:16:10 -0600

    Solve problem 45

--- src/main/scala/sbosley/euler/math/MathHelpers.scala
+++ src/main/scala/sbosley/euler/math/MathHelpers.scala
@@ -15,7 +15,14 @@ object MathHelpers {
   def quadraticFormula(a: Double, b: Double, c: Double): (Double, Double) = {
     // (-b +/- sqrt(b^2 - 4ac) / 2a)
     val sqrtTerm = Math.sqrt(b * b - 4 * a * c)
-    ((-b + sqrtTerm) / 2 * a, (-b - sqrtTerm) / 2 * a)
+    ((-b + sqrtTerm) / (2 * a), (-b - sqrtTerm) / (2 * a))
+  }
+
+  // Finds integer solutions to an^2 + bn + c
+  def quadraticFormulaIntegers(a: Long, b: Long, c: Long): Set[Long] = {
+    val (sol1, sol2) = quadraticFormula(a, b, c)
+    Set(sol1.toLong + 1, sol1.toLong, sol1.toLong - 1, sol2.toLong + 1, sol2.toLong, sol2.toLong - 1)
+      .filter(x => a * x * x + b * x + c == 0)
   }
 
   def factorial(n: Long): Long = {
---
+++ src/main/scala/sbosley/euler/p1to50/p41to50/problem45/TriangularPentagonalHexagonal.scala
@@ -0,0 +1,34 @@
+package sbosley.euler.p1to50.p41to50.problem45
+
+import sbosley.euler.math.MathHelpers
+
+object TriangularPentagonalHexagonal {
+
+  def main(args: Array[String]): Unit = {
+    // T285 = P165 = H143 = 40755
+    val thing = Stream.iterate(144L)(_ + 1).find(n => {
+      isPentagonal(hexagonal(n)) && isTriangular(hexagonal(n))
+    })
+    println(thing.map(hexagonal))
+  }
+
+  private def isHexagonal(n: Long): Boolean = {
+    val maybeSolutions = MathHelpers.quadraticFormulaIntegers(2, -1, -n)
+    maybeSolutions.exists(x => hexagonal(x) == n)
+  }
+
+  private def isPentagonal(n: Long): Boolean = {
+    val maybeSolutions = MathHelpers.quadraticFormulaIntegers(3, -1, -2 * n)
+    maybeSolutions.exists(x => x > 0 && pentagonal(x) == n)
+  }
+
+  private def isTriangular(n: Long): Boolean = {
+    val maybeSolutions = MathHelpers.quadraticFormulaIntegers(1, 1, -2 * n)
+    maybeSolutions.exists(x => triangular(x) == n)
+  }
+
+  private def triangular(x: Long): Long = MathHelpers.sumToN(x)
+  private def pentagonal(x: Long): Long = x * (3 * x - 1) / 2
+  private def hexagonal(x: Long): Long = x * (2 * x - 1)
+
+}
